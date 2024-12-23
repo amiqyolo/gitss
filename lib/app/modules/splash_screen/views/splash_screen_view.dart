991e10cf412940e9d3659daa49c55cce7d9f5303
Amiq Dian <amiqyolo@gmail.com>
2024-12-23 12:37:16 +0700

    slicing splash screen

--- lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -1,22 +1,91 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:gitss/app/modules/home/views/home_view.dart';
 
 import '../controllers/splash_screen_controller.dart';
 
 class SplashScreenView extends GetView<SplashScreenController> {
   const SplashScreenView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashScreenView'),
-        centerTitle: true,
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.off(() => HomeView());
+      },
+    );
+
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(90, 196, 161, 1),
+            Color.fromRGBO(77, 171, 196, 1),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text(
-          'SplashScreenView is working',
-          style: TextStyle(fontSize: 20),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/gitss_logo.svg',
+                semanticsLabel: 'Gitss Logo',
+                height: 76,
+              ),
+            ),
+            Positioned(
+              bottom: 24,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Developed By :',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(height: 14.0),
+                    Container(
+                      width: 234,
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/gulabed_logo.png',
+                            height: 26,
+                            alignment: Alignment.center,
+                          ),
+                          const SizedBox(width: 10.0),
+                          Image.asset(
+                            'assets/images/shiffu_logo.png',
+                            height: 26,
+                            alignment: Alignment.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
