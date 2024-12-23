d54491750669a04e9416096dab00acf6879a100f
Amiq Dian <amiqyolo@gmail.com>
2024-12-23 12:35:59 +0700

    add home view

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
+  static const HOME = _Paths.HOME;
 }
 
 abstract class _Paths {
   _Paths._();
   static const SPLASH_SCREEN = '/splash-screen';
+  static const HOME = '/home';
 }
